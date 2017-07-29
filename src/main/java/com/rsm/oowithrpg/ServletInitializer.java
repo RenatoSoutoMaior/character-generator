3b727a94ea11156098e6539d4287ff0b57a34f9d
RenatoSoutoMaior <renatosoutomaior18@gmail.com>
2017-07-29 11:35:19 -0300

    Generated initial files with Spring

---
+++ src/main/java/com/rsm/oowithrpg/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.rsm.oowithrpg;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(OowithrpgApplication.class);
+	}
+
+}
