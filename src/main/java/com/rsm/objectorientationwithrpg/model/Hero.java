c6d9d622eceabd84e85ac2e1de5fba2635804b4f
RenatoSoutoMaior <renatosoutomaior18@gmail.com>
2017-07-29 13:49:00 -0300

    Hero class implementation

--- src/main/java/com/rsm/objectorientationwithrpg/model/Hero.java
+++ src/main/java/com/rsm/objectorientationwithrpg/model/Hero.java
@@ -1,4 +1,95 @@
 package com.rsm.objectorientationwithrpg.model;
 
-public class Hero {
+public abstract class Hero {
+
+    private Integer vitality;
+
+    private Integer stamina;
+
+    private Integer strength;
+
+    private Integer dexterity;
+
+    private Integer endurance;
+
+    private Integer intelligence;
+
+    private Integer agility;
+
+    private Integer alertness;
+
+    private States states;
+
+    public Integer getVitality() {
+        return vitality;
+    }
+
+    public void setVitality(Integer vitality) {
+        this.vitality = vitality;
+    }
+
+    public Integer getStamina() {
+        return stamina;
+    }
+
+    public void setStamina(Integer stamina) {
+        this.stamina = stamina;
+    }
+
+    public Integer getStrength() {
+        return strength;
+    }
+
+    public void setStrength(Integer strength) {
+        this.strength = strength;
+    }
+
+    public Integer getDexterity() {
+        return dexterity;
+    }
+
+    public void setDexterity(Integer dexterity) {
+        this.dexterity = dexterity;
+    }
+
+    public Integer getEndurance() {
+        return endurance;
+    }
+
+    public void setEndurance(Integer endurance) {
+        this.endurance = endurance;
+    }
+
+    public Integer getIntelligence() {
+        return intelligence;
+    }
+
+    public void setIntelligence(Integer intelligence) {
+        this.intelligence = intelligence;
+    }
+
+    public Integer getAgility() {
+        return agility;
+    }
+
+    public void setAgility(Integer agility) {
+        this.agility = agility;
+    }
+
+    public Integer getAlertness() {
+        return alertness;
+    }
+
+    public void setAlertness(Integer alertness) {
+        this.alertness = alertness;
+    }
+
+    public States getStates() {
+        return states;
+    }
+
+    public void setStates(States states) {
+        this.states = states;
+    }
+
 }
--- src/main/java/com/rsm/objectorientationwithrpg/model/States.java
+++ src/main/java/com/rsm/objectorientationwithrpg/model/States.java
@@ -1,4 +1,9 @@
 package com.rsm.objectorientationwithrpg.model;
 
 public enum States {
+
+    IDLE("Idle"), WALKING("Walking"), RUNNING("Running"), JUMPING("Jumping"), ATTACKING("Attacking"), DIE("Die");
+
+    States(String description) {
+    }
 }
