e12c6a04534f8e31e0b2ecc81994bbbcc4c0eead
Renato Souto Maior <renatosoutomaior18@gmail.com>
2018-07-26 16:03:58 -0300

    Improving View for Endpoint Usage

--- src/main/resources/static/app/characters.controller.js
+++ src/main/resources/static/app/characters.controller.js
@@ -11,20 +11,65 @@
         var vm = this;
 
         vm.characters = [];
-        vm.getAll = getAll;
+
+        vm.genders = [
+            {name: "Male", value: 'MALE'},
+            {name: "Female", value: 'FEMALE'}];
+
+        vm.breeds = [
+            {name: "Alchemist", value: 'ALCHEMIST'},
+            {name: "Assassin", value: 'ASSASSIN'},
+            {name: "Barbarian", value: 'BARBARIAN'},
+            {name: "Bard", value: 'BARD'},
+            {name: "Cleric", value: 'CLERIC'},
+            {name: "Druid", value: 'DRUID'},
+            {name: "Elf", value: 'ELF'},
+            {name: "Khajiit", value: 'KHAJIIT'},
+            {name: "Lancer", value: 'LANCER'},
+            {name: "Magician", value: 'MAGICIAN'},
+            {name: "Monk", value: 'MONK'},
+            {name: "Pirate", value: 'PIRATE'},
+            {name: "Ranger", value: 'RANGER'},
+            {name: "Reptilian", value: 'REPTILIAN'},
+            {name: "Rogue", value: 'ROGUE'},
+            {name: "Shaman", value: 'SHAMAN'},
+            {name: "Thief", value: 'THIEF'},
+            {name: "Warrior", value: 'WARRIOR'}];
+
+        vm.getAllCharacters = getAllCharacters;
+        vm.create = create;
+        vm.remove = remove;
+
         init();
-        
+
         function init() {
-            console.log(vm);
-            getAll();
+            getAllCharacters();
         }
 
-        function getAll() {
+        function getAllCharacters() {
             var url = "/characters";
             var charactersPromise = $http.get(url);
             charactersPromise.then(function (response) {
                 vm.characters = response.data;
             });
+        }
+
+        function create(name, gender, breed) {
+            var url = "/characters?name=" + name + "&gender=" + gender + "&breed=" + breed;
+            var charactersPromise = $http.post(url, {headers: {'Content-Type': 'application/json'}});
+            charactersPromise.then(function (response) {
+                vm.characters = response.data;
+            });
+            getAllCharacters();
+        }
+
+        function remove(id) {
+            var url = "/characters/" + id;
+            var charactersPromise = $http.delete(url);
+            charactersPromise.then(function (response) {
+                vm.characters = response.data;
+            });
+            getAllCharacters();
         }
     }
 })();
