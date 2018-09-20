ddf9ca1e628990ac58055d9a6aff2844037b730a
Renato Souto Maior <renatosoutomaior18@gmail.com>
2018-09-20 16:54:44 -0300

    Creating DTO and Improving Resource / Front

--- src/main/java/com/rsm/charactergenerator/resource/CharacterResource.java
+++ src/main/java/com/rsm/charactergenerator/resource/CharacterResource.java
@@ -1,11 +1,11 @@
 package com.rsm.charactergenerator.resource;
 
-import com.rsm.charactergenerator.model.Breed;
 import com.rsm.charactergenerator.model.Character;
-import com.rsm.charactergenerator.model.Gender;
+import com.rsm.charactergenerator.model.CharacterDTO;
 import com.rsm.charactergenerator.service.CharacterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,14 +16,17 @@ import static java.util.Objects.isNull;
 @RestController
 public class CharacterResource {
 
-    private static final String NO_HUMAN_FOUND = "No Character found with ID ";
+    private static final String NO_HUMAN_FOUND = "No Character found with ID {}";
+
+    private CharacterService characterService;
 
     @Autowired
-    private CharacterService characterService;
+    public CharacterResource(CharacterService characterService) {
+        this.characterService = characterService;
+    }
 
     @GetMapping("/characters")
     public ResponseEntity<Object> getCharacters() {
-
         List<Character> characters = characterService.getAll();
 
         if (characters.isEmpty()) {
@@ -35,24 +38,22 @@ public class CharacterResource {
 
     @GetMapping("/characters/{id}")
     public ResponseEntity<Object> getCharacter(@PathVariable("id") Long id) {
-
         Character character = characterService.get(id);
 
         if (isNull(character)) {
-            return new ResponseEntity<>(NO_HUMAN_FOUND + id + ".", HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(String.format(NO_HUMAN_FOUND, id), HttpStatus.NOT_FOUND);
         }
 
         return new ResponseEntity<>(character, HttpStatus.OK);
     }
 
-    @PostMapping(value = "/characters", consumes = "application/json")
-    public ResponseEntity<Object> createCharacter(@RequestParam String name, Gender gender, Breed breed) {
-
-        if (isNull(name) || isNull(gender) || isNull(breed)) {
+    @PostMapping(value = "/characters", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Object> createCharacter(@RequestBody CharacterDTO characterDTO) {
+        if (isNull(characterDTO.getName()) || isNull(characterDTO.getGender()) || isNull(characterDTO.getBreed())) {
             return new ResponseEntity<>("Error trying to create a new Character. Please check data included.", HttpStatus.BAD_REQUEST);
         }
 
-        characterService.create(new Character(name, gender, breed));
+        characterService.create(new Character(characterDTO.getName(), characterDTO.getGender(), characterDTO.getBreed()));
         return new ResponseEntity<>("New Character created successfully.", HttpStatus.CREATED);
     }
 
