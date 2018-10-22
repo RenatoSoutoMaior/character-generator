bb07d99ea99374806645d756b562a29ef782a051
Renato Souto Maior <renatosoutomaior18@gmail.com>
2018-10-22 12:59:24 -0300

    Indentation

--- src/main/java/com/rsm/charactergenerator/CharacterGeneratorApplication.java
+++ src/main/java/com/rsm/charactergenerator/CharacterGeneratorApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CharacterGeneratorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CharacterGeneratorApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CharacterGeneratorApplication.class, args);
+    }
 }
